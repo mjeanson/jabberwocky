0e3b834d5dbd5d811be032cdd5ab7ae4a784791c
Alexandre Montplaisir <alexmonthy@efficios.com>
2017-05-24 18:28:47 -0400

    Remove the Type enum from FieldValue
    Sealed class are already a king of enum, we don't need the separate enum
    declaration, and we can already do:
    
      when(field) {
        is type1 -> ...
        is type2 -> ...
        etc.
      }
    
    Signed-off-by: Alexandre Montplaisir <alexmonthy@efficios.com>

--- jabberwocky-core/src/main/kotlin/com/efficios/jabberwocky/trace/event/FieldValue.kt
+++ jabberwocky-core/src/main/kotlin/com/efficios/jabberwocky/trace/event/FieldValue.kt
@@ -11,22 +11,10 @@ package com.efficios.jabberwocky.trace.event
 
 import java.util.*
 
-sealed class FieldValue(val type: Type,
-                        open val attributes: Map<String, String>?) {
-
-    enum class Type {
-        INTEGER,
-        FLOAT,
-        STRING,
-        ENUMERATION,
-        ARRAY,
-        STRUCTURE
-    }
-
-}
+sealed class FieldValue(open val attributes: Map<String, String>?) {}
 
 data class ArrayValue<T : FieldValue>(val elements: Array<T>,
-                                      override val attributes: Map<String, String>?) : FieldValue(Type.ARRAY, attributes) {
+                                      override val attributes: Map<String, String>?) : FieldValue(attributes) {
     val size: Int = elements.size
 
     fun getElement(index: Int): T {
@@ -51,20 +39,20 @@ data class ArrayValue<T : FieldValue>(val elements: Array<T>,
 
 data class EnumValue(val stringValue: String,
                      val longValue: Long,
-                     override val attributes: Map<String, String>?) : FieldValue(Type.ENUMERATION, attributes) {
+                     override val attributes: Map<String, String>?) : FieldValue(attributes) {
 
     override fun toString() = stringValue + '(' + longValue.toString() + ')'
 }
 
 data class FloatValue(val value: Double,
-                      override val attributes: Map<String, String>?) : FieldValue(Type.FLOAT, attributes) {
+                      override val attributes: Map<String, String>?) : FieldValue(attributes) {
 
     override fun toString() = value.toString()
 }
 
 data class IntegerValue(val value: Long,
                         val base: Int = 10,
-                        override val attributes: Map<String, String>?) : FieldValue(Type.INTEGER, attributes) {
+                        override val attributes: Map<String, String>?) : FieldValue(attributes) {
 
     override fun toString() = if (base == 16) {
         "0x" + java.lang.Long.toHexString(value)
@@ -74,13 +62,13 @@ data class IntegerValue(val value: Long,
 }
 
 data class StringValue(val value: String,
-                       override val attributes: Map<String, String>?) : FieldValue(Type.STRING, attributes) {
+                       override val attributes: Map<String, String>?) : FieldValue(attributes) {
 
     override fun toString() = value
 }
 
 data class StructValue(val elements: Map<String, FieldValue>,
-                       override val attributes: Map<String, String>?) : FieldValue(Type.STRUCTURE, attributes) {
+                       override val attributes: Map<String, String>?) : FieldValue(attributes) {
 
     val fieldNames = elements.keys
 
