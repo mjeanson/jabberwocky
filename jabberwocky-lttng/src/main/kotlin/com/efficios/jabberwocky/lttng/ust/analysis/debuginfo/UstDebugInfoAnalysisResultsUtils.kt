54c3f68e017facba90a97386a73fde0ddaeeebd8
Alexandre Montplaisir <alexmonthy@efficios.com>
2017-07-23 19:29:48 -0400

    lttng: Add LTTng-UST debug info analysis
    Imported from Scope.
    
    Signed-off-by: Alexandre Montplaisir <alexmonthy@efficios.com>

---
+++ jabberwocky-lttng/src/main/kotlin/com/efficios/jabberwocky/lttng/ust/analysis/debuginfo/UstDebugInfoAnalysisResultsUtils.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2017 EfficiOS Inc., Alexandre Montplaisir <alexmonthy@efficios.com>
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package com.efficios.jabberwocky.lttng.ust.analysis.debuginfo
+
+import com.efficios.jabberwocky.lttng.ust.trace.LttngUstTrace
+import com.efficios.jabberwocky.trace.event.FieldValue
+import com.efficios.jabberwocky.trace.event.ITraceEvent
+
+/**
+ * Extensions functions for UstDebugInfoAnalysisResults. To merge into that
+ * class once it moves to the Kotlin package.
+ */
+
+fun UstDebugInfoAnalysisResults.getCallsiteOfEvent(event: ITraceEvent): BinaryCallsite? {
+    /* This aspect only supports UST traces */
+    val trace = event.trace as? LttngUstTrace ?: return null
+    val layout = trace.ustEventLayout
+
+    /* We need both the vpid and ip contexts */
+    val vpid = event.getField(layout.contextVpid(), FieldValue.IntegerValue::class.java)?.value ?: return null
+    val ip = event.getField(layout.contextIp(), FieldValue.IntegerValue::class.java)?.value ?: return null
+    val ts = event.timestamp
+
+    return getBinaryCallsite(vpid, ts, ip)
+}
+
+/**
+ * Get the binary callsite (which means binary file and offset in this file)
+ * corresponding to the given instruction pointer, for the given PID and
+ * timetamp.
+ *
+ * @param pid
+ *            The PID for which we want the symbol
+ * @param ts
+ *            The timestamp of the query
+ * @param ip
+ *            The instruction pointer address
+ * @return The {@link BinaryCallsite} object with the relevant information
+ */
+fun UstDebugInfoAnalysisResults.getBinaryCallsite(pid: Long, ts: Long, ip: Long): BinaryCallsite? {
+    val file = getMatchingFile(ts, pid, ip) ?: return null
+
+    // TODO If path prefixes or other conversion had to be done it would be done here
+    // String fullPath = (trace.getSymbolProviderConfig().getActualRootDirPath() + file.getFilePath());
+    val fullPath = file.filePath
+
+    val offset = if (file.isPic) {
+        ip - file.baseAddress
+    } else {
+        /*
+         * In the case of the object being non-position-independent, we
+         * must pass the actual 'ip' address directly to addr2line.
+         */
+        ip
+    }
+    return BinaryCallsite(fullPath, file.buildId, offset, file.isPic)
+}
