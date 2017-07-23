66f723a7b3feecfb127988b1894ba089ec663c14
Alexandre Montplaisir <alexmonthy@efficios.com>
2017-07-23 07:35:41 -0400

    lttng: Integrate UST event layouts
    Signed-off-by: Alexandre Montplaisir <alexmonthy@efficios.com>

--- jabberwocky-lttng/src/main/kotlin/com/efficios/jabberwocky/lttng/ust/trace/LttngUstTrace.kt
+++ jabberwocky-lttng/src/main/kotlin/com/efficios/jabberwocky/lttng/ust/trace/LttngUstTrace.kt
@@ -12,6 +12,10 @@ package com.efficios.jabberwocky.lttng.ust.trace
 import com.efficios.jabberwocky.ctf.trace.CtfTrace
 import com.efficios.jabberwocky.ctf.trace.event.CtfTraceEvent
 import com.efficios.jabberwocky.ctf.trace.generic.GenericCtfTraceEventFactory
+import com.efficios.jabberwocky.ctf.trace.getTracerMajorVersion
+import com.efficios.jabberwocky.ctf.trace.getTracerMinorVersion
+import com.efficios.jabberwocky.ctf.trace.getTracerName
+import com.efficios.jabberwocky.lttng.ust.trace.layout.*
 import com.efficios.jabberwocky.trace.TraceInitializationException
 import java.nio.file.Path
 
@@ -24,4 +28,25 @@ class LttngUstTrace(tracePath: Path) : CtfTrace<CtfTraceEvent>(tracePath) {
     }
 
     override val eventFactory = GenericCtfTraceEventFactory(this)
+
+    val ustEventLayout: ILttngUstEventLayout by lazy {
+        val defaultLayout = LttngUst20EventLayout.getInstance()
+        val tracerName = getTracerName()
+        val majorVersion = getTracerMajorVersion() ?: -1
+        val minorVersion = getTracerMinorVersion() ?: -1
+
+        val layout: ILttngUstEventLayout = when (tracerName) {
+            "lttng-ust" -> when {
+                majorVersion >= 2 -> when {
+                    minorVersion >= 9 -> LttngUst29EventLayout.getInstance()
+                    minorVersion >= 8 -> LttngUst28EventLayout.getInstance()
+                    minorVersion >= 7 -> LttngUst27EventLayout.getInstance()
+                    else -> LttngUst20EventLayout.getInstance()
+                }
+                else -> defaultLayout
+            }
+            else -> defaultLayout
+        }
+        layout
+    }
 }
