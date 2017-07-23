ccd6d19430522b622a9ebdec35a4dfc2dd40da95
Alexandre Montplaisir <alexmonthy@efficios.com>
2017-07-23 06:21:28 -0400

    lttng: Integrate the kernel event layouts into the traces
    Signed-off-by: Alexandre Montplaisir <alexmonthy@efficios.com>

--- jabberwocky-lttng/src/main/kotlin/com/efficios/jabberwocky/lttng/kernel/trace/LttngKernelTrace.kt
+++ jabberwocky-lttng/src/main/kotlin/com/efficios/jabberwocky/lttng/kernel/trace/LttngKernelTrace.kt
@@ -12,6 +12,10 @@ package com.efficios.jabberwocky.lttng.kernel.trace
 import com.efficios.jabberwocky.ctf.trace.CtfTrace
 import com.efficios.jabberwocky.ctf.trace.event.CtfTraceEvent
 import com.efficios.jabberwocky.ctf.trace.generic.GenericCtfTraceEventFactory
+import com.efficios.jabberwocky.ctf.trace.getTracerMajorVersion
+import com.efficios.jabberwocky.ctf.trace.getTracerMinorVersion
+import com.efficios.jabberwocky.ctf.trace.getTracerName
+import com.efficios.jabberwocky.lttng.kernel.trace.layout.*
 import com.efficios.jabberwocky.trace.TraceInitializationException
 import java.nio.file.Path
 
@@ -20,8 +24,31 @@ class LttngKernelTrace(tracePath: Path) : CtfTrace<CtfTraceEvent>(tracePath) {
     init {
         /* Make sure the CTF metadata advertises "domain = kernel" */
         val domain = environment["domain"] ?: throw TraceInitializationException()
-        if (!domain.equals("\"kernel\"")) throw TraceInitializationException()
+        if (domain != "\"kernel\"") throw TraceInitializationException()
     }
 
     override val eventFactory = GenericCtfTraceEventFactory(this)
+
+    val kernelEventLayout: ILttngKernelEventLayout by lazy {
+        val defaultLayout = LttngEventLayout.getInstance()
+        val tracerName = getTracerName()
+        val majorVersion = getTracerMajorVersion() ?: -1
+        val minorVersion = getTracerMinorVersion() ?: -1
+
+        val layout: ILttngKernelEventLayout = when (tracerName) {
+            "perf" -> PerfEventLayout.getInstance()
+            "lttng-modules" -> when {
+                majorVersion >= 2 -> when {
+                    minorVersion >= 9 -> Lttng29EventLayout.getInstance()
+                    minorVersion >= 8 -> Lttng28EventLayout.getInstance()
+                    minorVersion >= 7 -> Lttng27EventLayout.getInstance()
+                    minorVersion >= 6 -> Lttng26EventLayout.getInstance()
+                    else -> LttngEventLayout.getInstance()
+                }
+                else -> defaultLayout
+            }
+            else -> defaultLayout
+        }
+        layout
+    }
 }
--- jabberwocky-lttng/src/test/kotlin/com/efficios/jabberwocky/lttng/kernel/trace/LttngKernelTraceTest.kt
+++ jabberwocky-lttng/src/test/kotlin/com/efficios/jabberwocky/lttng/kernel/trace/LttngKernelTraceTest.kt
@@ -9,17 +9,22 @@
 
 package com.efficios.jabberwocky.lttng.kernel.trace
 
+import com.efficios.jabberwocky.lttng.kernel.trace.layout.Lttng28EventLayout
+import com.efficios.jabberwocky.lttng.kernel.trace.layout.LttngEventLayout
 import com.efficios.jabberwocky.lttng.testutils.ExtractedCtfTestTrace
 import com.efficios.jabberwocky.trace.TraceInitializationException
 import org.eclipse.tracecompass.testtraces.ctf.CtfTestTrace
 import org.junit.ClassRule
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class LttngKernelTraceTest {
 
     companion object {
         @JvmField @ClassRule
         val KERNEL_TRACE = ExtractedCtfTestTrace(CtfTestTrace.KERNEL)
+        @JvmField @ClassRule
+        val KERNEL_TRACE2 = ExtractedCtfTestTrace(CtfTestTrace.MANY_THREADS)
         @JvmField @ClassRule
         val NON_KERNEL_TRACE = ExtractedCtfTestTrace(CtfTestTrace.CYG_PROFILE)
     }
@@ -34,5 +39,16 @@ class LttngKernelTraceTest {
     fun testOpeningNonKernelTrace() {
         val path = NON_KERNEL_TRACE.trace.tracePath
         LttngKernelTrace(path)
+    }
+
+    @Test
+    fun testEventLayout() {
+        val path1 = KERNEL_TRACE.trace.tracePath
+        val trace1 = LttngKernelTrace(path1)
+        assertEquals(LttngEventLayout.getInstance(), trace1.kernelEventLayout)
+
+        val path2 = KERNEL_TRACE2.trace.tracePath
+        val trace2 = LttngKernelTrace(path2)
+        assertEquals(Lttng28EventLayout.getInstance(), trace2.kernelEventLayout)
     }
 }
