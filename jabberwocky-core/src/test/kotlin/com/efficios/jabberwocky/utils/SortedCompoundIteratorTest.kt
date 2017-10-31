2de2aa45249694622638cc121a6ee4a779405ef2
Alexandre Montplaisir <alexmonthy@efficios.com>
2017-10-31 13:59:42 -0400

    core: Add RewindingSortedCompoundIterator
    New iterator wrapper, similar to the SortedCompoundIterator, except it
    will also be able to seek backwards.
    
    Switching direction is relatively costly, but common use cases will be
    of either going forwards or backwards for long streaks.
    
    Signed-off-by: Alexandre Montplaisir <alexmonthy@efficios.com>

--- jabberwocky-core/src/test/kotlin/com/efficios/jabberwocky/utils/SortedCompoundIteratorTest.kt
+++ jabberwocky-core/src/test/kotlin/com/efficios/jabberwocky/utils/SortedCompoundIteratorTest.kt
@@ -9,155 +9,16 @@
 
 package com.efficios.jabberwocky.utils
 
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
-import org.junit.Test
 import java.util.*
 
 /**
- * Tests for the [SortedCompoundIterator]
+ * Tests for the [SortedCompoundIterator].
  */
-class SortedCompoundIteratorTest {
+class SortedCompoundIteratorTest : SortedCompoundIteratorTestBase() {
 
-    companion object {
-
-        private val list1 = listOf(1, 3, 5)
-        private val list2 = listOf(2, 4, 6)
-        private val list3 = listOf(4, 5, 6)
-        private val list4 = listOf(2, 2, 2)
-
-        private val customList1 = listOf(
-                MyObject("A"),
-                MyObject("B"),
-                MyObject("C"))
-
-        private val customList2 = listOf(
-                MyObject("a"),
-                MyObject("b"),
-                MyObject("c"))
+    override fun <T> provideCompoundIterator(iterators: Collection<Iterator<T>>, comparator: Comparator<T>): Iterator<T> {
+        return SortedCompoundIterator(iterators, comparator)
     }
 
-    /**
-     * Test that an error is correctly reported if there are less than 1
-     * iterator.
-     */
-    @Test(expected = IllegalArgumentException::class)
-    fun testNoIter() {
-        SortedCompoundIterator(emptyList<Iterator<String>>(), naturalOrder())
-    }
-
-    /**
-     * Test a compound iterator with just 1 iterator, basically just a wrapper.
-     */
-    @Test
-    fun test1Iter() {
-        val iters = listOf(list1.iterator())
-        val sci = SortedCompoundIterator(iters, naturalOrder())
-        testIteratorContents(sci, 1, 3, 5)
-    }
-
-    /**
-     * Test a compound iterator with 2 iterators, requiring to sort the values.
-     */
-    @Test
-    fun test2Iters() {
-        val iters = listOf(list1, list2).map { it.iterator() }
-        val sci = SortedCompoundIterator(iters, naturalOrder())
-        testIteratorContents(sci, 1, 2, 3, 4, 5, 6)
-    }
-
-    /**
-     * Test that identical values coming from the same iterator are correctly
-     * reported in the global iteration.
-     */
-    @Test
-    fun testIdenticalValues1Iter() {
-        val iters = listOf(list1, list4).map { it.iterator() }
-        val sci = SortedCompoundIterator(iters, naturalOrder())
-        testIteratorContents(sci, 1, 2, 2, 2, 3, 5)
-    }
-
-    /**
-     * Test a compound iterator with separate iterators that hold identical
-     * (compareTo() == 0) values. The duplicate values should be present in the
-     * iteration.
-     */
-    @Test
-    fun testIdenticalValues2Iters() {
-        val iters = listOf(list2, list3).map { it.iterator() }
-        val sci = SortedCompoundIterator(iters, naturalOrder())
-        testIteratorContents(sci, 2, 4, 4, 5, 6, 6)
-    }
-
-    /**
-     * Test that upon exhaustion, the iterator correctly throws a
-     * [NoSuchElementException] if we try to read from it.
-     */
-    @Test(expected = NoSuchElementException::class)
-    fun testExhaustion() {
-        val iter1 = list1.iterator()
-        val iters = listOf(iter1)
-        val sci = SortedCompoundIterator(iters, naturalOrder())
-
-        sci.next()
-        sci.next()
-        sci.next()
-        sci.next()
-    }
-
-    // ------------------------------------------------------------------------
-    // Tests with custom objects and comparators
-    // ------------------------------------------------------------------------
-
-    private data class MyObject(val value: String)
-
-    /**
-     * Test a compound iterator of custom objects
-     */
-    @Test
-    fun testCustomObjects1() {
-        val iters = listOf(customList1, customList2).map { it.iterator() }
-        val sci = SortedCompoundIterator(iters, compareBy { it.value })
-
-        /* Default string comparator places capitals first */
-        testIteratorContents(sci,
-                MyObject("A"),
-                MyObject("B"),
-                MyObject("C"),
-                MyObject("a"),
-                MyObject("b"),
-                MyObject("c"))
-    }
-
-    /**
-     * Test a compound iterator of custom objects with a more complex
-     * comparator.
-     */
-    @Test
-    fun testCustomObjects2() {
-        val iters = listOf(customList2, customList1).map { it.iterator() }
-        val comparator = Comparator<MyObject> { o1, o2 -> o1.value.compareTo(o2.value, ignoreCase = true) }
-        val sci = SortedCompoundIterator(iters, comparator)
-
-        /*
-         * Using custom comparator here. Note we put the contents of customList2
-         * first, so they will show up first in the compound iterator.
-         */
-        testIteratorContents(sci,
-                MyObject("a"),
-                MyObject("A"),
-                MyObject("b"),
-                MyObject("B"),
-                MyObject("c"),
-                MyObject("C"))
-    }
-
-    @SafeVarargs
-    private fun <T> testIteratorContents(iter: SortedCompoundIterator<T, Iterator<T>>, vararg values: T) {
-        for (value in values) {
-            assertEquals(value, iter.next())
-        }
-        assertFalse(iter.hasNext())
-    }
 
 }
